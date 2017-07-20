ac1ed40987de633e318b723317d865872a8cac28
yangyk <yyk0623@126.com>
2017-07-20 21:36:36 +0800

    improve: soft for Pruning
    add sort for Pruning. becom faster, but still fail 4 examples.

--- 416. Partition Equal Subset Sum/main.c
+++ 416. Partition Equal Subset Sum/main.c
@@ -1,27 +1,44 @@
-bool rec(int* nums, int sizen, int sizem, int dep, int sum, int exp) {
+int* array;
+int count;
+int exps;
+int rec(int sizen, int dep, int sum) {
     int i;
     bool ret;
-    for(i=sizen;i<sizem;i++) {
-        if(sum+nums[i]==exp)
-            return true;
+    for(i=sizen;i<count;i++) {
+        if(sum+array[i]==exps)
+            return 0;
+        if(sum+array[i]>exps)
+            return 1;
         if(dep) {
-            ret = rec(nums, i+1, sizem, dep-1, sum+nums[i], exp);
-            if(ret)
+            ret = rec(i+1, dep-1, sum+array[i]);
+            if(!ret)
                 return ret;
         }
     }
-    return false;
+    return 2;
 }
 bool canPartition(int* nums, int numsSize) {
     int sum = 0;
-    int i;
-    bool ret;
+    int i,j;
+    int ret;
+    array = nums;
+    count = numsSize;
+    for(i=0;i<numsSize-1;i++)
+        for(j=i+1;j<numsSize;j++)
+            if(array[i]<array[j]) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+    //for(i=0;i<numsSize;i++)
+    //    printf("%d ",array[i]);
     for(i=0; i<numsSize; i++)
         sum += nums[i];
     if(sum & 1)
         return false;
-    ret = rec(nums, 0, numsSize, numsSize/2, 0, sum/2);
-    if(ret)
-        return ret;
+    exps = sum/2;
+    ret = rec(0, numsSize/2, 0);
+    if(!ret)
+        return true;
     return false;
 }
