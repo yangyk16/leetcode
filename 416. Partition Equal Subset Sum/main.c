0e9f704747ca8d20d4c320790f3291c78a6585c4
yangyk <yyk0623@126.com>
2017-07-20 21:06:21 +0800

    improve recurrent
    limit dept <= num/2, highly improve efficient, but still now fast enough

--- 416. Partition Equal Subset Sum/main.c
+++ 416. Partition Equal Subset Sum/main.c
@@ -1,22 +1,27 @@
-bool canPartition(int* nums, int numsSize) {
-    volatile int* array = malloc(pow(2,numsSize)*4);
-    int i,j;
-    int sum=0;
-    for(i=0;i<numsSize;i++)
-        sum+=nums[i];
-    if(sum&1)
-        return false;
-    else
-        sum/=2;
-    for(i=0;i<pow(2,numsSize);i++)
-        array[i]=0;
-    for(i=0;i<numsSize;i++) {
-        int jmax = pow(2, i);
-        for(j=0;j<jmax;j++) {
-            array[j+jmax-1]=array[j]+nums[i];
-            if(array[j+jmax-1] == sum)
-                return true;
+bool rec(int* nums, int sizen, int sizem, int dep, int sum, int exp) {
+    int i;
+    bool ret;
+    for(i=sizen;i<sizem;i++) {
+        if(sum+nums[i]==exp)
+            return true;
+        if(dep) {
+            ret = rec(nums, i+1, sizem, dep-1, sum+nums[i], exp);
+            if(ret)
+                return ret;
         }
     }
+    return false;
+}
+bool canPartition(int* nums, int numsSize) {
+    int sum = 0;
+    int i;
+    bool ret;
+    for(i=0; i<numsSize; i++)
+        sum += nums[i];
+    if(sum & 1)
+        return false;
+    ret = rec(nums, 0, numsSize, numsSize/2, 0, sum/2);
+    if(ret)
+        return ret;
     return false;
 }
